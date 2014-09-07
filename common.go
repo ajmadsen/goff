b60fd9418a431b5e937bc5db7b066fce6292221f
Adam Madsen <madsen.adam@gmail.com>
2014-09-07 15:51:55 -0500

    Add errors to constants

--- common.go
+++ common.go
@@ -8,15 +8,27 @@ package ff
 #include <stdlib.h>
 */
 import "C"
-import (
-	"errors"
-)
+import "syscall"
+
+type CodecID int
+
+type Error struct {
+	Errno int
+}
+
+func (e Error) Error() string {
+	var errbuf [1024]C.char
+	C.av_make_error_string(&errbuf[0], C.size_t(len(errbuf)), C.int(e.Errno))
+	return C.GoStringN(&errbuf[0], C.int(len(errbuf)))
+}
+
+func AVError(err syscall.Errno) Error {
+	return Error{-int(err)}
+}
 
 // averror converts a return code to a descriptive string
 func averror(ret C.int) error {
-	var errbuf [1024]C.char
-	C.av_make_error_string(&errbuf[0], C.size_t(len(errbuf)), ret)
-	return errors.New(C.GoStringN(&errbuf[0], C.int(len(errbuf))))
+	return Error{int(ret)}
 }
 
 // Init initializes the FFmpeg library for usage
